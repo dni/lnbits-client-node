1c1e447cd47d84a67d844c1a984633313d308364
dni ⚡ <office@dnilabs.com>
2023-07-04 13:06:08 +0200

    update client

---
+++ api/apis.ts
@@ -0,0 +1,15 @@
+export * from './coreApi';
+import { CoreApi } from './coreApi';
+export * from './coreApiInterface'
+import * as http from 'http';
+
+export class HttpError extends Error {
+    constructor (public response: http.IncomingMessage, public body: any, public statusCode?: number) {
+        super('HTTP request failed');
+        this.name = 'HttpError';
+    }
+}
+
+export { RequestFile } from '../model/models';
+
+export const APIS = [CoreApi];
